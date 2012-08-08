242f49b0f110a8ee94c245584be64826434a5ba6
Robby Grossman <robby@freerobby.com>
2012-08-08 10:39:03 -0400

    Add a monit recipe to monitor and restart riak, for use in conjunction with the monit cookbook. Monit implementation has its own recipe, so it will not affect any nodes that do not specifically add it to their run list.

---
+++ attributes/monit.rb
@@ -0,0 +1,4 @@
+default.riak.monit.process_matching = "/usr/lib/riak/erts-5.8.5/bin/beam.smp"
+default.riak.monit.start_program = "/usr/sbin/riak"
+default.riak.monit.http_host = "localhost"
+default.riak.monit.http_port = "8098"
---
+++ recipes/monit.rb
@@ -0,0 +1,14 @@
+# This is an optional recipe that provides monit monitoring of your riak node. Requires the monit cookbook.
+
+include_recipe "monit::default"
+
+monitrc "riak" do
+  variables(
+    :process_matching => node[:riak][:monit][:process_matching],
+    :start_program => node[:riak][:monit][:start_program],
+    :http_host => node[:riak][:monit][:http_host],
+    :http_port => node[:riak][:monit][:http_port]
+  )
+  template_source "riak-monit.conf.erb"
+  template_cookbook "riak"
+end
