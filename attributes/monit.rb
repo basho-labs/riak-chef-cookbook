34219ad68bca7dd56feee1f8fd778b1f08fc088d
Hector Castro <hectcastro@gmail.com>
2014-01-16 19:48:12 -0500

    Remove monit support.

--- attributes/monit.rb
+++
@@ -1,4 +0,0 @@
-default['riak']['monit']['process_matching'] = "#{node['riak']['data_dir']}/erts-5.8.5/bin/beam.smp"
-default['riak']['monit']['start_program'] = "#{node['riak']['bin_dir']}/riak"
-default['riak']['monit']['http_host'] = "localhost"
-default['riak']['monit']['http_port'] = "8098"
